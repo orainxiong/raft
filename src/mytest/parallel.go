d818ba2bf18bc63db0f9b66bbc6b4e2556bf3a79
orainxiong <orain.xiong@gmail.com>
2016-08-04 16:51:18 +0800

    add peer and peer heartbeat

--- src/mytest/parallel.go
+++ src/mytest/parallel.go
@@ -12,7 +12,6 @@ const (
 	timeout     = 1
 	Running     = "running"
 	Stopped     = "stopped"
-	OK          = "ok"
 	Follower    = "follower"
 	WorkBacklog = 256
 )
@@ -34,6 +33,79 @@ func NewEvent() *Event {
 	}
 }
 
+type Peer struct {
+	sync.RWMutex
+	name         string
+	state        string
+	stopped      chan bool
+	lastActivity time.Time
+	server       *Server
+}
+
+func NewPeer(name string, server *Server) *Peer {
+	return &Peer{
+		name:    name,
+		stopped: make(chan bool),
+		server:  server,
+	}
+}
+
+func (this *Peer) State() string {
+	this.RLock()
+	defer this.RUnlock()
+	return this.state
+}
+
+func (this *Peer) SetState(state string) {
+	this.Lock()
+	defer this.Unlock()
+	this.state = state
+}
+
+func (this *Peer) GetState() string {
+	this.RLock()
+	defer this.RUnlock()
+	return fmt.Sprintf("peer %s state %s", this.name, this.state)
+}
+
+func (this *Peer) setLastActivity(now time.Time) {
+	this.Lock()
+	defer this.Unlock()
+	this.lastActivity = now
+}
+
+func (this *Peer) StartHeartbeat() {
+
+	startup := make(chan bool)
+	this.server.RoutineWaitGroup.Add(1)
+	go func() {
+		defer this.server.RoutineWaitGroup.Done()
+		this.heartbeat(startup)
+	}()
+	<-startup
+}
+
+func (this *Peer) StopHeartbeat() {
+	close(this.stopped)
+	this.SetState(Stopped)
+}
+
+func (this *Peer) heartbeat(startup chan bool) {
+	ticker := time.Tick(timeout * time.Second)
+	startup <- true
+	for {
+		select {
+		case <-ticker:
+			log.Printf("peer %s heatbeat", this.name)
+			this.setLastActivity(time.Now())
+		case <-this.stopped:
+			log.Printf("peer %s stop", this.name)
+			this.SetState(Stopped)
+			return
+		}
+	}
+}
+
 type ParallelBase interface {
 	Do(*Event) error
 	Send(*Event) error
@@ -48,6 +120,7 @@ type Server struct {
 	RoutineWaitGroup sync.WaitGroup
 	workchan         chan *Event
 	stopped          chan bool
+	peers            map[string]*Peer
 }
 
 func NewServer() *Server {
@@ -56,6 +129,7 @@ func NewServer() *Server {
 		role:     Follower,
 		workchan: make(chan *Event, WorkBacklog),
 		stopped:  make(chan bool),
+		peers:    make(map[string]*Peer),
 	}
 }
 
@@ -87,12 +161,23 @@ func (this *Server) Start() {
 		defer this.RoutineWaitGroup.Done()
 		this.loop()
 	}()
+
+	for _, peer := range this.peers {
+		log.Printf("peer %s startheartbeat", peer.name)
+		peer.StartHeartbeat()
+	}
 }
 
 func (this *Server) Stop() {
 	if Stopped == this.State() {
 		return
 	}
+
+	for _, peer := range this.peers {
+		log.Printf("peer %s stopheartbeat", peer.name)
+		peer.StopHeartbeat()
+	}
+
 	close(this.stopped)
 	this.RoutineWaitGroup.Wait()
 	this.SetState(Stopped)
--- src/mytest/parallel_test.go
+++ src/mytest/parallel_test.go
@@ -1,33 +1,39 @@
 package mytest
 
 import (
-	"testing"
 	"log"
+	"testing"
+	"time"
 )
 
 func TestNewServer(t *testing.T) {
 	/*
-	测试并发模型
-		1. server 基于 waitgroup 记录所有运行的 goroutine. 当退出的时候通过 wait 方式,等待所有的 goroutine 退出
-		2. 所有的 goroutine 通过 server 的 stop channel 实时trace server 的状态
-		3. event 中存放 channel error
-			3.1 通过 channel 传递 error
-			3.2 通过 channel 做到同步等待 event 的返回.以确定 event 确实被消费掉
+		测试并发模型
+			1. server 基于 waitgroup 记录所有运行的 goroutine. 当退出的时候通过 wait 方式,等待所有的 goroutine 退出
+			2. 所有的 goroutine 通过 server 的 stop channel 实时trace server 的状态
+			3. event 中存放 channel error
+				3.1 通过 channel 传递 error
+				3.2 通过 channel 做到同步等待 event 的返回.以确定 event 确实被消费掉
 	*/
-	log.SetFlags(log.Ldate|log.Ltime|log.Lshortfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	server := NewServer()
+
+	peer := NewPeer("xuyuan", server)
+
+	server.peers[peer.name] = peer
 
 	server.Start()
 	defer server.Stop()
 
 	var (
-		e *Event
+		e   *Event
 		err error
 	)
 	e = NewEvent()
-	log.Printf("%#v",e)
+	log.Printf("%#v", e)
 	err = server.Do(e)
-	log.Printf("%#v , err %s",e, err)
+	log.Printf("%#v , err %s", e, err)
 
 
+	time.Sleep(10 * time.Second)
 }
